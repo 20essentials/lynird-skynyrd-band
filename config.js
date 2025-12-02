7d856e99c55cbd56ced54fa3f094529369f99bea
DominicDev <s0l0estudi4r@gmail.com>
2025-12-02 12:41:53 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' LYNYRD SKYNYRD';
+export let nameSongs = [
+	"FREE BIRD", /*1*/
+	"SWEET HOME ALABAMA", /*2*/
+	"SIMPLE MAN", /*3*/
+	"TUESDAY'S GONE", /*4*/
+	"GIMME THREE STEPS", /*5*/
+	"CALL ME THE BREEZE", /*6*/
+	"THAT SMELL", /*7*/
+	"I NEED YOU", /*8*/
+	"CRY FOR THE BAD MAN", /*9*/
+	"ON THE HUNT", /*10*/
+	"FREE BIRD (SHADE TREE DEMO)", /*11*/
+	"THINGS GOIN' ON", /*12*/
+	"I AIN'T THE ONE", /*13*/
+	"WHAT'S YOUR NAME", /*14*/
+	"DON'T ASK ME NO QUESTIONS", /*15*/
+	"SWAMP MUSIC", /*16*/
+	"DOUBLE TROUBLE", /*17*/
+	"RAILROAD SONG", /*18*/
+	"MISSISSIPPI KID", /*19*/
+	"I NEVER DREAMED", /*20*/
+]
